79fcf9500db6884ddf86aff809b32d790b08ae24
Le Duy Thuc <leduythuc.cs@gmail.com>
2021-05-19 22:18:48 +0700

    Print more information when compiler output is too long

--- dmoj/executors/compiled_executor.py
+++ dmoj/executors/compiled_executor.py
@@ -191,8 +191,8 @@ class CompiledExecutor(BaseExecutor, metaclass=_CompiledExecutorMeta):
         limit = env.compiler_output_character_limit
         try:
             output = safe_communicate(process, None, outlimit=limit, errlimit=limit)[self.compile_output_index]
-        except OutputLimitExceeded:
-            output = b'compiler output too long (> %d kiB)' % (limit // 1024)
+        except OutputLimitExceeded as e:
+            output = b'compiler output too long (> %d kiB)\nMore informations: ' % (limit // 1024) + str(e).encode()
 
         if self.is_failed_compile(process):
             if process.is_tle:
