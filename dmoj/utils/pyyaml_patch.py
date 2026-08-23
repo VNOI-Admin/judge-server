8fc045a1a0fa92bb1573fc3bcdadb206459f0d44
Le Bao Hiep <baohiep2013@gmail.com>
2026-08-23 18:39:36 +0700

    pyyaml_patch: ignore mypy final-attribute error

--- dmoj/utils/pyyaml_patch.py
+++ dmoj/utils/pyyaml_patch.py
@@ -3,6 +3,6 @@ import re
 import yaml.reader
 
 if yaml.reader.Reader.NON_PRINTABLE.pattern == '[^\t\n\r -~\x85\xa0-\ud7ff\ue000-\ufffd]':
-    yaml.reader.Reader.NON_PRINTABLE = re.compile(
+    yaml.reader.Reader.NON_PRINTABLE = re.compile(  # type: ignore[misc]
         '[^\x09\x0a\x0d\x20-\x7e\x85\xa0-\ud7ff\ue000-\ufffd\U00010000-\U0010ffff]'
     )
